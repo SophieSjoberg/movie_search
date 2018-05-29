43d053769efc333b895962e73fe6ee1046bcbbc3
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-05-29 22:28:06 +0200

    Add movie search controller with index and search actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post :search, controller: :movie_search, action: :search
+  root controller: :movie_search, action: :index
 end
